5554403683ffe526db791dd694c5b1f66284e069
dungdq <dqdung96@gmail.com>
2026-01-22 00:50:44 +0700

    commit code

--- src/main/java/vn/scrip/buoi38_bvn/controller/CartController.java
+++ src/main/java/vn/scrip/buoi38_bvn/controller/CartController.java
@@ -29,17 +29,31 @@ public class CartController {
 
     @PostMapping("/add/{id}")
     public String add(@PathVariable Long id, HttpSession session) {
-        Book book = bookService.findById(id); // ✅ SỬA
+
+        Book book = bookService.findById(id);
         if (book == null) return "redirect:/books";
 
         List<CartItem> cart = (List<CartItem>) session.getAttribute("cart");
-        if (cart == null) cart = new ArrayList<>();
 
-        cart.add(new CartItem(book, 1));
+        if (cart == null) {
+            cart = new ArrayList<>();
+        }
+        boolean found = false;
+        for (CartItem item : cart) {
+            if (item.getBook().getId().equals(id)) {
+                item.setQuantity(item.getQuantity() + 1);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            cart.add(new CartItem(book, 1));
+        }
+
         session.setAttribute("cart", cart);
-
         return "redirect:/cart";
     }
+
 
     @PostMapping("/checkout")
     public String checkout(HttpSession session) {
@@ -63,4 +77,26 @@ public class CartController {
         session.removeAttribute("cart");
         return "redirect:/reader/borrows";
     }
+
+    @PostMapping("/cart/update")
+    @ResponseBody
+    public void updateQuantity(@RequestParam Long bookId,
+                               @RequestParam int quantity,
+                               HttpSession session) {
+
+        List<CartItem> cart =
+                (List<CartItem>) session.getAttribute("cart");
+
+        if (cart == null) return;
+
+        for (CartItem item : cart) {
+            if (item.getBook().getId().equals(bookId)) {
+                item.setQuantity(quantity);
+                break;
+            }
+        }
+
+        session.setAttribute("cart", cart);
+    }
+
 }
